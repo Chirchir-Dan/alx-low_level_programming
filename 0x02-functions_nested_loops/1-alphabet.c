8b005a94dc5292378a8d7e5ad3fd9b7b441e598a
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-12 14:22:08 -0700

    Added c program 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+	/**
+	 *alphabet - print letters of the alphabet in lowercase
+	 */
+
+
+	void alphabet(void)
+	{
+		char c;
+
+
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+
+
+		_putchar('\n');
+	}
+
