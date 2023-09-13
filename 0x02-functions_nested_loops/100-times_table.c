33e46c14b998d2491703406b4a1a5dabb24a78c2
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-13 12:53:54 -0700

    removed trailing white spaces

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,77 +1,45 @@
-100-times_table.c#include "main.h"
-
-/**
- * print_single_digit - Print a single digit.
- * @num: The digit to print.
- */
-
-void print_single_digit(int num);
-
-/**
- * print_double_digit - Print a double-digit number.
- * @num: The double-digit number to print.
- */
-
-void print_double_digit(int num);
+#include "main.h"
 
 /**
  * print_times_table - Print the times table for n, starting from 0.
  * @n: The desired times table (0-15).
  */
-
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return; /* Invalid input, do nothing */
+	int row, column;
 
-	for (int row = 0; row <= n; row++)
+	if (n < 0 || n > 15)
+		return;
+
+	for (row = 0; row <= n; row++)
 	{
-		for (int column = 0; column <= n; column++)
+		for (column = 0; column <= n; column++)
 		{
+			int product = row * column;
+
 			if (column > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				if (product < 100)
+				{
+					_putchar(' ');
+					if (product < 10)
+						_putchar(' ');
+				}
 			}
-
-			int product = column * row;
 
 			if (product < 10)
-			{
 				_putchar(' ');
-				_putchar(' ');
-				print_single_digit(product);
-			}
-
 			else
 			{
-				print_double_digit(product);
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
 			}
 
+			_putchar((product % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
-}
-
-/**
- * print_single_digit - Print a single digit.
- * @num: The digit to print.
- */
-
-void print_single_digit(int num)
-{
-	_putchar(num + '0');
-}
-
-/**
- * print_double_digit - Print a double-digit number.
- * @num: The double-digit number to print.
- */
-
-void print_double_digit(int num)
-{
-	_putchar((num / 10) + '0');
-	_putchar((num % 10) + '0');
 }
 
