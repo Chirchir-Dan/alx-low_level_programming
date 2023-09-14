192934dadf400e1723dc5911e9e1ffb92f773561
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-14 13:48:15 -0700

    added code 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
+	else if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('0' + n);
+	}
+}
+
