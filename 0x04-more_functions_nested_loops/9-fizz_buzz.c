62d204a03e4457adbe4494dba64bd7e32723bab9
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-14 13:12:38 -0700

    edited code 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,37 +7,33 @@
  */
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			/* If i is divisible by both 3 and 5, print "FizzBuzz" */
 			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			/* If i is divisible by 3, print "Fizz" */
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			/* If i is divisible by 5, print "Buzz" */
 			printf("Buzz");
 		}
 		else
 		{
-			/* If i is not divisible by 3 or 5, print the number itself */
 			printf("%d", i);
 		}
 
 		if (i < 100)
 		{
-			/* Print a space after the value unless it's the last one */
 			printf(" ");
 		}
 		else
 		{
-			/* Print a newline character to start a new line */
 			printf("\n");
 		}
 	}
