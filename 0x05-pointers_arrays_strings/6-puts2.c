24b462b4a45efd91b270b2dfadd3d1369c98b5d6
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-18 13:58:31 -0700

    edited betty style

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,28 +8,28 @@
  */
 void puts2(char *str)
 {
-    int length = 0;
-    int temp = 0;
-    char *current = str;
-    int index;
+	int length = 0;
+	int temp = 0;
+	char *current = str;
+	int index;
 
-    /* Calculate the length of the string */
-    while (*current != '\0')
-    {
-        current++;
-        length++;
-    }
+	/* Calculate the length of the string */
+	while (*current != '\0')
+	{
+		current++;
+		length++;
+	}
 
-    temp = length - 1;
+	temp = length - 1;
 
-    for (index = 0; index <= temp; index++)
-    {
-        if (index % 2 == 0)
-        {
-            _putchar(str[index]);
-        }
-    }
+	for (index = 0; index <= temp; index++)
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
