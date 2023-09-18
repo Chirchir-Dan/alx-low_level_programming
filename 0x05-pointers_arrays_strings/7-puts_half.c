a02bb4ca4a3028d72bd8e9ecf9afee0424501fea
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-18 14:02:31 -0700

    edited betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,25 +8,25 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int start;
+	int length = 0;
+	int start;
 
-    /* Calculate the length of the string */
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+	/* Calculate the length of the string */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Calculate the starting index for the second half */
-    start = (length - 1) / 2 + 1;
+	/* Calculate the starting index for the second half */
+	start = (length - 1) / 2 + 1;
 
-    /* Print the second half of the string */
-    while (str[start] != '\0')
-    {
-        putchar(str[start]);
-        start++;
-    }
+	/* Print the second half of the string */
+	while (str[start] != '\0')
+	{
+		putchar(str[start]);
+		start++;
+	}
 
-    putchar('\n'); /* Print a new line */
+	putchar('\n'); /* Print a new line */
 }
 
