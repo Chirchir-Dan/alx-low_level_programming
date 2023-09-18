59927d8e9bd27dd95cfc94eb88fd7ec2b896a6dc
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-18 14:07:58 -0700

    Added code 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _strcpy - Copies a string including the null byte.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: A pointer to the destination buffer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';  /* Append the null byte to the destination string */
+
+	return dest;
+}
+
