6850ad78e14c45584b72fe3e65581a830d90e789
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-21 06:40:34 -0700

    Re-wrote code 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,37 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string to be appended to dest.
+ * _strcat - Concatenates two strings
+ * @dest: The destination string to which the source string will be appended
+ * @src: The source string that will be appended to the destination
  *
- * Return: A pointer to the resulting string dest.
+ * Return: A pointer to the destination string `dest`
  */
 char *_strcat(char *dest, char *src)
 {
-	char *dest_ptr = dest;
+    int dest_index;  /* Index for the destination string */
+    int src_index;   /* Index for the source string */
 
-	/* Find the end of the dest string */
-	while (*dest_ptr)
-		dest_ptr++;
-
-	/* Copy characters from src to dest until the end of src is reached */
-	while (*src)
-	{
-		*dest_ptr = *src;/* Copy the character from src to dest */
-		dest_ptr++;/* Move the destination pointer forward */
-		src++;/* Move the source pointer forward */
+    /* Find the null-terminated end of the destination string */
+    dest_index = 0;
+    while (dest[dest_index] != '\0')
+    {
+        dest_index++;
     }
-	}
-	/* Add a terminating null byte to the concatenated string */
-	*dest_ptr = '\0';
 
-	/* Return a pointer to the resulting string dest */	
-	return dest;
+    /* Copy characters from src to the end of dest */
+    src_index = 0;
+    while (src[src_index] != '\0')
+    {
+        dest[dest_index] = src[src_index];
+        dest_index++;
+        src_index++;
+    }
+
+    /* Add a null terminator to the end of the concatenated string */
+    dest[dest_index] = '\0';
+
+    /* Return a pointer to the concatenated string (dest) */
+    return (dest);
 }
 
