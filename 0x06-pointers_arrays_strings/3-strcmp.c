00ada0e8d5e7242395501904360dbdc746b68cec
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-21 12:26:35 -0700

    re-wrote code 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,23 @@
 #include "main.h"
 /**
- * _strcmp - Compare the values of two strings
- * @s1: The first string for comparison
- * @s2: The second string for comparison
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * Return: The difference between the ASCII values of the first differing characters
+ * Return: s1[i] - s2[i]
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int index;
+	int i;
 
-	index = 0;
-	while (s1[index] != '\0' && s2[index] != '\0')
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[index] != s2[index])
+		if (s1[i] != s2[i])
 		{
-			return (s1[index] - s2[index]);
+			return (s1[i] - s2[i]);
 		}
-		index++;
+		i++;
 	}
 	return (0);
 }
