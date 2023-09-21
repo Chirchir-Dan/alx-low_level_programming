7bc39eb81b17a06751d5dfbbd8a3330381dd782e
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-21 11:46:15 -0700

    Added code 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - Compare the values of two strings
+ * @s1: The first string for comparison
+ * @s2: The second string for comparison
+ *
+ * Return: The difference between the ASCII values of the first differing characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+
+	index = 0;
+	while (s1[index] != '\0' && s2[index] != '\0')
+	{
+		if (s1[index] != s2[index])
+		{
+			return (s1[index] - s2[index]);
+		}
+		index++;
+	}
+	return (0);
+}
+
