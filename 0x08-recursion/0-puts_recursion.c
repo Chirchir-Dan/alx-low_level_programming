27e5ff7ea836b061f0849f2bc11dfdbc1a69a2ba
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-27 08:32:44 -0700

    removed leading white spaces to comply with Betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') /* End of string, print newline */
-    {
-        putchar('\n');
-        return;
-    }
+	if (*s == '\0') /* End of string, print newline */
+	{
+		putchar('\n');
+		return;
+	}
 
-    putchar(*s); /* Print current character */
-    _puts_recursion(s + 1); /* Recursive call with next character */
+	putchar(*s); /* Print current character */
+	_puts_recursion(s + 1); /* Recursive call with next character */
 }
 
