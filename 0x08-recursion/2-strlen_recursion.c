3312b9b45ccaf5fa7147d342896af5443de63e2f
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-27 09:59:08 -0700

    Added parenthesis to return statements

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,9 @@ int _strlen_recursion(char *s)
 {
 	/* Base case: when we reach the null terminator, the length is 0 */
 	if (*s == '\0')
-		return 0;
+		return (0);
 
 	/* Recursively call _strlen_recursion with the next character */
-	return 1 + _strlen_recursion(s + 1);
+	return (1) + _strlen_recursion(s + 1);
 }
 
