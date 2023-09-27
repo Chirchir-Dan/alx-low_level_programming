d6c64a4a15967c769f4d856d7a610222fbad01f0
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-09-27 12:02:54 -0700

    Added description to the function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/*
+/**
  * _pow_recursion - Calculates x raised to the power of y.
+ *
  * @x: The base.
  * @y: The exponent.
  *
- * This function calculates the result of x raised to the power of y.
- * If y is negative, the function returns -1 to indicate an error.
+ * Description: This function calculates the result of x raised to the power of y.
+ *              If y is negative, the function returns -1 to indicate an error.
  *
  * Return: The result of x^y, or -1 if y is less than 0 (error).
  */
