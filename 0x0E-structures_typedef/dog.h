064fbf47601c3f6b80a746722c9de1e9fc9727fa
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-10-09 13:16:14 -0700

    Defined a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog structure
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the owner's name
+ *
+ * Description: This struct defines a dog's characteristics
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
