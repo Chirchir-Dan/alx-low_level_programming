1999b103f9db3d3e2b38f598678a6ee6f8eb4731
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-10-10 13:07:11 -0700

    used malloc on 1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,43 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog - Initializes a struct dog
- * @d: Pointer to the struct dog to initialize
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+		{
+			/* Handle memory allocation failure */
+			return;
+		}
+	}
 
-	d->name = name;
+	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		free(d); /* Clean up memory if allocation fails */
+		return;
+	}
+	strcpy(d->name, name);
+
 	d->age = age;
-	d->owner = owner;
+
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name); /* Clean up memory if allocation fails */
+		free(d);
+		return;
+	}
+	strcpy(d->owner, owner);
 }
 
