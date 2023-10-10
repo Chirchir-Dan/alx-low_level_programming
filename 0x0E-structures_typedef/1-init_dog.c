6d95ca638eadded746e294829f59311e9be9aa8d
Kipkosgei, Daniel <1danchirchir@gmail.com>
2023-10-10 12:42:23 -0700

    changed way of assigning pointers to name and owner

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * init_dog - Initializes a variable of type struct dog
- * @d: Pointer to struct dog to initialize
- * @name: Name to initialize
- * @age: Age to initialize
- * @owner: Owner to initialize
- *
- * Return: (void)
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
-
-	d->name = malloc(sizeof(char) * (strlen(name) + 1));
-	if (d->name == NULL)
-		return;
-
-	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (d->owner == NULL)
-	{
-		free(d->name);
-		return;
-	}
-
-	strcpy(d->name, name);
+		d = malloc(sizeof(struct dog));
+	d->name = name;
 	d->age = age;
-	strcpy(d->owner, owner);
+	d->owner = owner;
 }
 
