f3355fcaf2151631428b0311ba61fb332037be06
Chirchir-Dan <1danchirchir@gmail.com>
2023-10-24 01:36:41 -0700

    removed whites spaces

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,12 @@
-section .data
-	hello db "Hello, Holberton", 0   ; Null-terminated string
-	format db "%s", 10, 0           ; Format string for printf
-
-section .text
-	global _start
+global _start
+	extern printf
 
 _start:
-	; Call printf
-	mov rdi, format
-	mov rsi, hello
-	xor rax, rax    ; Clear RAX to indicate no vector registers are used
+	mov rdi, hello_format
+	xor rax, rax
 	call printf
+	mov rax, 0
+	ret
 
-	; Exit the program
-	mov rax, 60     ; syscall number for exit
-	xor rdi, rdi    ; status = 0
-	syscall
-
-section .bss
-	section .bss
-l_hello resb 64
+hello_format: db "Hello, Holberton", 10, 0
 
