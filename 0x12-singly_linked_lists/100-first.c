8a7f28499af19c8e0bb7e993826522914ce70680
Chirchir-Dan <1danchirchir@gmail.com>
2023-10-24 01:19:46 -0700

    resolved all warnings

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,14 @@
 /**
  * before_main - Prints a message before the main function is executed.
  *
- * This function is executed before the main function and prints a message.
+ * Description: This function is executed before the main function and
+ * prints a message.
  */
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("I bore my house upon my back!\n");
 }
 
