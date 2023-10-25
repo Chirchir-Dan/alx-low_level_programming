86755a7b951f4f53f8a6f5043963166681352d2b
Chirchir-Dan <1danchirchir@gmail.com>
2023-10-25 14:19:01 -0700

    removed unnecessary indentation erros

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	while (*head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
 		current = *head;
 		*head = current->next;
 		free(current);
 	}
 
-	*head = NULL; 
+	*head = NULL;
 }
 
