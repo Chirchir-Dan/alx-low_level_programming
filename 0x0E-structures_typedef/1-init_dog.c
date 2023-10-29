ccd9e470abff5caff7c9fdfa9bb14f938449001d
Kipkosgei Daniel <85983789+Chirchir-Dan@users.noreply.github.com>
2023-10-29 20:36:28 +0000

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "dog.h"
-
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
+ * init_dog - Initializes a variable of type struct dog
+ * @d:       Pointer to a struct dog to initialize
+ * @name:    Name to initialize
+ * @age:     Age to initialize
+ * @owner:   Owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -22,23 +17,26 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		}
 	}
 
+	/* Allocate memory for 'name' and 'owner' */
 	d->name = malloc(strlen(name) + 1);
-	if (d->name == NULL)
-	{
-		free(d); /* Clean up memory if allocation fails */
-		return;
-	}
-	strcpy(d->name, name);
-
-	d->age = age;
-
 	d->owner = malloc(strlen(owner) + 1);
-	if (d->owner == NULL)
+
+	if (d->name == NULL || d->owner == NULL)
 	{
-		free(d->name); /* Clean up memory if allocation fails */
+		/* Handle memory allocation failure */
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
 		free(d);
 		return;
 	}
+
+	strcpy(d->name, name);
+	d->age = age;
 	strcpy(d->owner, owner);
 }
-
