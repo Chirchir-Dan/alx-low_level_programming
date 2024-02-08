e788b9fee3fc87a24d127bb2679b0d411ffbdf5e
Chirchir-Dan <1danchirchir@gmail.com>
2024-02-08 17:18:20 +0300

    Wrote a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,39 @@
-#include <stddef.h>
 #include "main.h"
+#include <stddef.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: A pointer to a string of 0 and 1 characters
+ * binary_to_uint - Convert a binary string to an unsigned integer.
+ * @b: A pointer to the binary string (0 and 1 characters).
  *
- * Return: the converted number, or 0 if:
- *	- b is NULL
- *	- atleast one character is not 0 or 1.
+ * Return: The converted number, or 0 if there are characters
+ * that are not '0' or '1' in the string @b or if @b is NULL.
  */
-
-unsigned int binary_to_uint (const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 	int i = 0;
 
 	if (b == NULL)
 		return (0);
-	/**
-	 * Iterate through each character of the string
-	 * untill the null terminator is reached
-	 */
 	while (b[i] != '\0')
 	{
 		if (b[i] == '0')
 		{
+			/* Shift the current result left by 1 and add 0. */
 			result = (result << 1);
 		}
 		else if (b[i] == '1')
 		{
+			/* Shift the current result left by 1 and add 1. */
 			result = (result << 1) | 1;
 		}
 		else
 		{
-			/*If char is not 0 or 1*/
+			/* If a character is not '0' or '1, return 0 as specified. */
 			return (0);
 		}
+		i++;
 	}
-	return (result);
 
+	return (result);
 }
