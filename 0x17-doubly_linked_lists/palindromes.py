2d0d855c96f73e407a57b15c281d2b0eb9e6741e
Chirchir-Dan <1danchirchir@gmail.com>
2024-03-24 16:48:18 +0300

    Removed line number from 102

--- 0x17-doubly_linked_lists/palindromes.py
+++ 0x17-doubly_linked_lists/palindromes.py
@@ -2,11 +2,15 @@
 def is_palindrome(num):
     return str(num) == str(num)[::-1]
 
-largest_palindrome = 0;
+largest_palindrome = 0
+factors = (0, 0)
 
 for i in range(100,1000):
-    for j in range(100, 1000):
+    for j in range(i, 1000):
         product = i * j
         if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
-print (largest_palindrome)
+            factors = (i, j)
+            
+print(largest_palindrome)
+print(factors)
