67cb4b51f3120b29f47dbdc992ca7bc843e8a97a
Chirchir-Dan <1danchirchir@gmail.com>
2024-07-17 22:51:57 +0300

    modified header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,15 +3,6 @@
 
 #include <stddef.h>
 
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int binary_search_exp(int *array, size_t left, size_t right, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-
 /**
  * struct listint_s - singly linked list
  *
@@ -27,5 +18,15 @@ typedef struct listint_s
     size_t index;
     struct listint_s *next;
 } listint_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_exp(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
 
 #endif
